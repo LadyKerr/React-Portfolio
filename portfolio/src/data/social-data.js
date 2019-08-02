4fcc548e0192ad5ae5288de6e76e2ae4501b2e75
LadyKerr <kadiekerr@gmail.com>
2019-08-01 21:51:36 -0400

    social data starter

---
+++ portfolio/src/data/social-data.js
@@ -0,0 +1,24 @@
+//import social icons from fontawesome
+
+const social = [
+  {
+    id: 1,
+    account: "Twitter",
+    profileURL: "",
+    icon: ""
+  },
+  {
+    id: 2,
+    account: "GitHub",
+    profileURL: "",
+    icon: ""
+  },
+  {
+    id: 3,
+    account: "LinkedIn",
+    profileURL: "",
+    icon: ""
+  }
+];
+
+export default social;
