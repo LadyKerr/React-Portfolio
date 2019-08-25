3a24109fc6a19cc5ab5327f0d2216a20d5b4aa17
LadyKerr <kadiekerr@gmail.com>
2019-08-25 18:42:45 -0400

    hid navbar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import Navbar from "./components/navbar/Navbar";
+//import Navbar from "./components/navbar/Navbar";
 import Home from "./components/Home/Home";
 import ProjectList from "./components/Projects/ProjectList";
 import Contact from "./components/Contact/Contact";
@@ -9,7 +9,7 @@ import "./app.css";
 function App() {
   return (
     <div className="app">
-      <Navbar />
+      {/* <Navbar /> */}
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/projects" component={ProjectList} />
